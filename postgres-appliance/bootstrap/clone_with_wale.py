627b93c654bd745fdbe7c4cbb6eb3a0f9aca7c11
Mwangi <8320816+kidynamit@users.noreply.github.com>
2019-11-07 17:01:13 +0200

    orderdict excepts expanded_size_bytes not last_modified

--- postgres-appliance/bootstrap/clone_with_wale.py
+++ postgres-appliance/bootstrap/clone_with_wale.py
@@ -69,7 +69,7 @@ def choose_backup(output, recovery_target_time):
         raise Exception("wal-e could not found any backups")
     match_timestamp = match = None
     for backup in backup_list:
-        last_modified = parse(backup['last_modified'])
+        last_modified = parse(backup['expanded_size_bytes'])
         if last_modified < recovery_target_time:
             if match is None or last_modified > match_timestamp:
                 match = backup
